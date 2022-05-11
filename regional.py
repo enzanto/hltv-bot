dffcbaeb9d2f0c2b91687b94a8881b40b9dc000e
enzanto <freddyg91@gmail.com>
2022-05-11 22:32:17 +0200

    added function that checks rank date, big rewrite

--- regional.py
+++ regional.py
@@ -1,3 +1,4 @@
+from calendar import month
 import time
 import logging
 from datetime import datetime
@@ -19,31 +20,34 @@ logging.debug("debug is active")
 print("debugging is set to: ", debug)
 #get date of latest ranking
 def rank_date():
-    top30 = 'https://www.hltv.org/ranking/teams/'
+    now = datetime.now()
+    month = now.strftime("%B")
+    day = now.strftime("%-d")
+    monday = int(day) - now.weekday()
+    top30 = "https://www.hltv.org/ranking/teams/2022/" + month.lower() + "/" + str(monday)
+    while requests.get(top30).status_code != 200:
+        time.sleep(2)
     req30 = requests.get(top30)
     tree30 = html.fromstring(req30.content)
     date = tree30.xpath('string(//div[@class="regional-ranking-header"]/text())').replace('CS:GO World ranking on ', '')
+    sanitized = date.split()
+    date = sanitized[0] + " " + sanitized[1] + " " + sanitized[2]
     time.sleep(0.2)
-    logging.DEBUG(f"retrieved the latest rank date: {date}")
+    logging.info("retrieved the latest rank date: %s", date)
     return date
 
 Client = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
 #get old tweets and check if the date from HLTV has been tweeted before
 def old_tweets():
-    if debug == True:
-        date = "debuggig"
-    elif debug == False:
-        date = rank_date()
+    date = rank_date()
     userID = Client.get_user(username="HLTV_NOR").data.id #change name to your twitter handle
     oldTweet = Client.get_users_tweets(id=userID).data
     num_tweets = len(oldTweet)
     for i in range(num_tweets):
         tweet = oldTweet[i].text
-        #print(tweet)
         if date in tweet:
             return True
-        else:
-            return date
+    return date
 # get the newest link to regional rankings for Norway
 def getRegionalLink():
     now = datetime.now()
@@ -52,21 +56,11 @@ def getRegionalLink():
     monday = int(day) - now.weekday()
     link = "https://www.hltv.org/ranking/teams/2022/" + month.lower() + "/" + str(monday) + "/country/Norway"
     #check if valid link
-    response = requests.get(link).status_code
-    if response == 404:
-        return response
+    while requests.get(link).status_code != 200:
+        time.sleep(5)
+    logging.info("returning regional link: %s", link)
     return link
-#check if date have been tweeted before. If HLTV has not yet updated the ranks, we will retry once a minute for 10 minutes.
 
-if old_tweets() == True or getRegionalLink() == 404:
-    logging.info("rank not ready")
-    for i in range(10):
-        if old_tweets() == True or getRegionalLink() == 404:
-            logging.info("Retrying to see if new rank availible, attempt: ", i + 1)
-            time.sleep(120)
-        else:
-            break
-date = old_tweets()
 
 def message():
     # find regional teams first
@@ -86,33 +80,38 @@ def message():
     url = teams.readlines()
     regionalRank = []
     teamrank = []
+    url_date = str(date).split()
+    url_month = url_date[0]
+    url_day = re.split("([0-9]+)", url_date[1])[1]
+    url_date_sanitized = url_month.lower() + "/" + url_day
     for regionalURL in regionalList:
         req = requests.get(regionalURL)
         tree = html.fromstring(req.content)
+        regional_rank_date = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/span/a')[0].attrib["href"]
+        while url_date_sanitized not in regional_rank_date:
+            logging.debug("regional loop")
+            req = requests.get(regionalURL)
+            tree = html.fromstring(req.content)
+            regional_rank_date = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/span/a')[0].attrib["href"]
+            time.sleep(5)
         teamname = tree.xpath('string(//h1[@class="profile-team-name text-ellipsis"]/text())')
         rank = tree.xpath('string(//html/body/div[2]/div/div[2]/div[1]/div/div[2]/div[2]/div[1]/span/a/text())')
-        if len(teamname) == 0:
-            logging.error("error in retrieving teamname")
-            return "error"
-        else:
-            if rank != "":
-                regionalRank.append(rank + " " + teamname)
-            else:
-                logging.info(teamname + " has no rank")
+        regionalRank.append(rank + " " + teamname)
+        logging.info(rank + " " + teamname + " " + regional_rank_date)
         time.sleep(0.5)
     for url in url:
         req = requests.get(url)
         tree = html.fromstring(req.content)
+        list_rank_date = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/span/a')[0].attrib["href"]
+        while url_date_sanitized not in list_rank_date:
+            req = requests.get(regionalURL)
+            tree = html.fromstring(req.content)
+            list_rank_date = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/span/a')[0].attrib["href"]
+            time.sleep(5)
         teamname = tree.xpath('string(//h1[@class="profile-team-name text-ellipsis"]/text())')
         rank = tree.xpath('string(//html/body/div[2]/div/div[2]/div[1]/div/div[2]/div[2]/div[1]/span/a/text())')
-        if len(teamname) == 0:
-            logging.error("error in retrieving teamname")
-            return "error"
-        else:
-            if rank != "":
-                teamrank.append(rank + " " + teamname)
-            else:
-                logging.info(teamname + " has no rank")
+        teamrank.append(rank + " " + teamname)
+        logging.info(rank + " " + teamname + " " + list_rank_date)
         time.sleep(0.5)
     regionalRankSorted = sorted(regionalRank, key=lambda rank: int(re.split(r'#| ', rank)[1]))
     teamrank_sorted = sorted(teamrank, key=lambda rank: int(re.split(r'#| ', rank)[1]))
@@ -141,40 +140,27 @@ def message():
 #sends the tweet, but will check for errors when we checked for team ranks.
 def send_tweet():
     tweet = message()
-    if "error" in tweet:
-        logging.error("error in fetching data")
-        for i in range(10):
-            time.sleep(10)
-            logging.info("retrying attempt ", i + 1)
-            tweet_checked = message()
-            if "error" not in tweet_checked:
-                for i in range(len(tweet_checked)):
-                    if i == 0:
-                        if debug == True:
-                            logging.debug(tweet_checked[i])
-                        elif debug == False:
-                            tweet_id = Client.create_tweet(text=tweet_checked[i]).data["id"]
-                    else:
-                        if debug == True:
-                            logging.debug(tweet_checked[i])
-                        elif debug == False:
-                            tweet_id = Client.create_tweet(in_reply_to_tweet_id=tweet_id, text=tweet_checked[i]).data["id"]
-
+    for i in range(len(tweet)):
+        if i == 0:
+            if debug == True:
+                logging.info(tweet[i])
+            elif debug == False:
+                tweet_id = Client.create_tweet(text=tweet[i]).data["id"]
+                logging.info(tweet[i])
+        else:
+            if debug == True:
+                logging.info(tweet[i])
+            elif debug == False:
+                tweet_id = Client.create_tweet(in_reply_to_tweet_id=tweet_id, text=tweet[i]).data["id"]
+                logging.info(tweet[i])
+if __name__ == '__main__':
+    date = old_tweets()
+    if date == True:
+        if debug == True:
+            logging.debug("old tweet found, but debugging is set to True")
+            date = rank_date()
+            send_tweet()
+        elif debug != True:
+            logging.error("Aborting, no new ranks found or tweet already sent")
     else:
-        for i in range(len(tweet)):
-            if i == 0:
-                if debug == True:
-                    logging.debug(tweet[i])
-                elif debug == False:
-                    tweet_id = Client.create_tweet(text=tweet[i]).data["id"]
-                    logging.info(tweet[i])
-            else:
-                if debug == True:
-                    logging.debug(tweet[i])
-                elif debug == False:
-                    tweet_id = Client.create_tweet(in_reply_to_tweet_id=tweet_id, text=tweet[i]).data["id"]
-                    logging.info(tweet[i])
-if date == True:
-    logging.error("Aborting, no new ranks found or tweet already sent")
-else:
-    send_tweet()
+        send_tweet()
